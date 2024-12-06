829213547f3ba81e4759f8a5938d78c5fa35f0a5
draw1 <90817893+draw1@users.noreply.github.com>
2024-12-06 21:30:43 +0700

    Update dashboard.php

--- tlunews/views/admin/dashboard.php
+++ tlunews/views/admin/dashboard.php
@@ -5,12 +5,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản trị viên</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <h1>Chào mừng Quản trị viên</h1>
-    <a href="/admin/news">Quản lý tin tức</a>
-    <a href="/admin/logout">Đăng xuất</a>
+    <header class="bg-primary text-white py-4 text-center">
+        <h1>Dashboard</h1>
+    </header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../admin/dashboard.php">Trang chủ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../admin/news/index.php">Quản Lý Tin tức</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../controllers/adminController.php?logout=true" ;>Đăng Xuất</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 </body>
 
 </html>
